15cac7b8a4c23a78c696c85cdc4161c8b123f94e
Ayanle Yusuf <ayanle226@gmail.com>
2025-05-22 23:54:13 -0500

    Completed day 2 of 100 of my project

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+import os
+from forms import LoginForm
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
 @app.route('/')
@@ -19,6 +22,15 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', books=books)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Logged in user: {form.username.data}, remember me? {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Login", form=form)
 
 @app.route('/about')
 def about():
